766f15c3553703339dd063df3823292dc3408cef
Ishita Gupta <64515632+Ishita19code@users.noreply.github.com>
2021-08-03 19:14:37 +0530

    Update Card.jsx

--- THA-21/Card.jsx
+++ THA-21/Card.jsx
@@ -1,20 +1,63 @@
 import "./styles.css";
+import { useState } from "react";
 
-const Card = ({fName, cal, list, setList}) =>
+const Card = ({obj, setList, list}) =>
 {
+  const [edit, setEdit] = useState(true);
+  const [fUpdated, setFUpdated] = useState("");
+  const [calUpdated, setCalUpdated] = useState("");
+
+  const updatedList = (updatedObj) =>
+  {
+      const update = list.map((item, index) => 
+        (item.id === updatedObj.id) ?
+          item[index] = updatedObj : item
+      )
+      setList(update);
+  };
+
   return( 
     <div className="card">
-      <h4 style={{paddingLeft:"10px", fontSize:"20px"}}>{fName}</h4>
-      <p style={{paddingLeft:"10px"}}>You have consumed {cal} calories today.</p>
-      <button className="btn_delete" onClick={(event) => 
+      {edit ? 
+      (<>
+        <h4 style={{paddingLeft:"10px", fontSize:"20px"}}>{obj.food}</h4>
+        <p style={{paddingLeft:"10px"}}>You have consumed {obj.calorie} calories today.</p>
+      </>) : 
+      <>  
+      <input 
+            type="text" 
+            placeholder="Update FoodItem"
+            className="inputFood"
+            value={fUpdated} 
+            onChange={(e) => setFUpdated(e.target.value)}/>
+
+      <input 
+            type="number" 
+            placeholder="Update Calorie"
+            className="inputFood"
+            value={calUpdated}
+            onChange={(e) => setCalUpdated(e.target.value)}/>
+      </>}
+
+      <button className="btn_delete" onClick={() => 
         {
-          const deletedItem = list.filter((item) => item !== fName);
-          setList(deletedItem);
-          event.preventDefault();
+          const newArray = list.filter((item) => item.id !== obj.id);
+          setList(newArray);
         }
         }>Delete</button>
 
-        <button className="btn_edit">Edit</button>
+        <button className="btn_edit" 
+        onClick={() => 
+        {
+          const updatedObj = 
+          {
+            id: obj.id,
+            food:fUpdated,
+            calorie:calUpdated
+          }
+          updatedList(updatedObj);
+          setEdit(!edit);
+        }}>{edit ? "Edit" : "Save"}</button>
     </div>
   ) 
 }
